35c33d57bbdba40c6ec9be02a4b23ad616542452
lankeshwhatsapp4444 <151029357+lankeshwhatsapp4444@users.noreply.github.com>
2024-11-02 14:16:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XNMgGbwb#4v_GKgqugDR3ldotNbchZrDb5wQHqvSvV5Rv4OF6nnk",
+SESSION_ID: process.env.SESSION_ID || "uAlRUabQ#n09Pejv7fSY3Wal9U1M4NcUd363zoxtjMTd8mTAGz2U",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
